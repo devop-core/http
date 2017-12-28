124293bfa9bd9202f208f3ab315aea904a5e134b
Zlatko Hristov <zlatko.2create@gmail.com>
2017-12-28 11:42:56 +0200

    Basic tests files

---
+++ tests/RequestTest.php
@@ -0,0 +1,7 @@
+<?php
+namespace DevOp\Core\Http\Test;
+
+class RequestTest extends \PHPUnit_Framework_TestCase
+{
+    
+}
---
+++ tests/ResponseTest.php
@@ -0,0 +1,6 @@
+<?php namespace DevOp\Core\Test\Http;
+
+class ResponseTest
+{
+    
+}
